4567886d51bef1ecff59f7a1aa940d4827182496
笒鬼鬼 <113222487+cenguigui@users.noreply.github.com>
2026-09-04 14:34:17 +0800

    Update common.js

--- assets/iframe/js/common.js
+++ assets/iframe/js/common.js
@@ -2,6 +2,7 @@
 layui.config({  // common.js是配置layui扩展模块的目录，每个页面都需要引入
     version: '318',   // 更新组件缓存，设为true不缓存，也可以设一个固定值
     defaultTheme: 'theme-colorful', // 默认主题
+    // base: getProjectUrl() + 'assets/iframe/module/'
     base: '/assets/iframe/module/'
 }).extend({
     steps: 'steps/steps',
